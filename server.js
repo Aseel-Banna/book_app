f771a832177bdb000ccac14f5ec80b0f56857ea0
ASEEL ALBANNA <ASEELNIHAD.AN@GMAIL.COM>
2021-03-02 11:57:22 +0200

    lab 12 is done

--- server.js
+++ server.js
@@ -26,13 +26,13 @@ server.get('/hello', (req, res) => {
 
 server.get("/books/:id", (req, res) => {
     let id = req.params.id;
-    //   let bookShelf = getBookShelf();
     let SQL = `SELECT * FROM books WHERE id=$1;`;
     let values = [id];
     client.query(SQL, values)
         .then((result) => {
             console.log('MAKE SURE',result.rows[0]);
             res.render('pages/books/details', { book :result.rows[0]});
+            // res.render('pages/books/show', { book :result.rows[0]});
         })
         .catch(() => {
             errorHandler('Error in getting Database');
@@ -44,25 +44,21 @@ server.get('/searches/new', (req, res) => {
 });
 
 server.post('/searches', (req, res) => {
-    // console.log('Hello from search');
     let searchInput = req.body.search;
     let key = process.env.GOOGLE_API_KEY;
     let url;
     if (req.body.searchValue === 'title') {
-        url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}+intitle`;
+        url = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${searchInput}`;
     } else {
-        url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}+inauthor`;
+        url = `https://www.googleapis.com/books/v1/volumes?q=+inauthor:${searchInput}`;
     }
-    // console.log('Hello from search22');
+    console.log('Hello from search22', url);
     superagent.get(url)
         .then(result => {
-            // console.log(result);
+            console.log(result.body);
             let booksArray = result.body.items.map((item) => {
                 return new Book(item);
             })
-            // console.log('ASEEL',booksArray);
-            // renderData(booksArray);
-            // res.send(booksArray);
             res.render('pages/searches/show', { books: booksArray });
         })
         .catch(() => {
@@ -86,14 +82,6 @@ server.get('/error', (req, res) => {
 })
 
 server.post('/books', (req, res) => {
-    // let SQL = `INSERT INTO books(author, title, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5)RETURNING id;`;
-    // let value = req.body;
-    // let safeValues = [value.author, value.title, value.isbn ,value.image_url, value.description];
-    // client.query(SQL, safeValues)
-    //     .then(results => {
-    //         console.log(results);
-    //         res.redirect( `/books/${results.rows[0].id}`);
-    //     })
     let newSQL = `INSERT INTO books (author, title, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5) RETURNING id;`;
     let newValues = [req.body.author, req.body.title, req.body.isbn, req.body.image_url, req.body.description];
   
@@ -124,9 +112,13 @@ function Book(data) {
     } else {
         this.image_url = "https://i.imgur.com/J5LVHEL.jpg";
     }
-    this.title = data.volumeInfo.title;
-    this.author = data.volumeInfo.authors;
-    this.description = data.volumeInfo.description || "There is no description";
-    this.isbn = (data.volumeInfo.industryIdentifiers && data.volumeInfo.industryIdentifiers[0].type + " " +
-        data.volumeInfo.industryIdentifiers[0].identifier) || "There is no isbn ";
+    this.title = (data.volumeInfo.title) ? data.volumeInfo.title : `Title unavilable`;
+    this.author = (Array.isArray(data.volumeInfo.authors)) ? data.volumeInfo.authors.join(', ') : `Unknown Author`;
+    this.description = (data.volumeInfo.description) ? data.volumeInfo.description : `description unavilable`;
+    // this.isbn = (data.volumeInfo.industryIdentifiers) ? data.volumeInfo.industryIdentifiers[0].identifier : `Unknown ISBN`;
+    if (data.volumeInfo.industryIdentifiers) {
+        this.isbn = data.volumeInfo.industryIdentifiers[0].identifier
+    } else {
+        this.isbn = "No ISBN for this book!!";
+    }
 }
