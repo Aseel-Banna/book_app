ffd65c4f3b23c69a227f7f43b622d562305a88d6
ASEEL ALBANNA <ASEELNIHAD.AN@GMAIL.COM>
2021-03-02 20:13:57 +0200

    lab 13 is done

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ server.set('view engine', 'ejs');
 
 const pg = require('pg');
 
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
 
 server.use(express.static('./public'));
 server.use(express.urlencoded({ extended: true }));
